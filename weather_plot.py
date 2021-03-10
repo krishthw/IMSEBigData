deda24d815666668ec8a95b082413f27e342df69
Krish W <53475147+krishthw@users.noreply.github.com>
2021-03-10 15:14:43 -0500

    Update weather_plot.py

--- weather_plot.py
+++ weather_plot.py
@@ -1,15 +1,16 @@
-# This is a code to generate line plot for weather data
+# This is a code to generate line plot/bar plot for weather data
 # need to have 4 columns "Date","Minimum Temperature","Mean Temperature","Maximum Temperature"
 # Date shoud be in %m/%d/%y format
 
 #import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
+import altair as alt
 
 # impoart data
-df=pd.read_csv("weatherdata.csv")
-
-# in case you have "no data." in columns change type "object" to "float64" , this will introduce NaN to any strings present
+df=pd.read_csv("January_66502.csv") # all data("2017_2021_weatherdata.csv")
+# in case you have "no data." in columns change type "object" to "float64"
+# this will introduce NaN to any strings present
 df['Minimum Temperature']=pd.to_numeric(df['Minimum Temperature'], errors='coerce')
 df['Mean Temperature']=pd.to_numeric(df['Mean Temperature'], errors='coerce')
 df['Maximum Temperature']=pd.to_numeric(df['Maximum Temperature'], errors='coerce')
@@ -24,17 +25,35 @@ meandf['MY']=pd.to_datetime(meandf['Month_Year'], format='%b %Y')
 # sort by month and year
 meandf=meandf.sort_values(by='MY')
 # delete the extra columns
-meandf.drop('MY', axis=1, inplace=True)
+# meandf.drop('MY', axis=1, inplace=True)
 meandf.set_index(meandf['Month_Year'],inplace=True)
 
 # save monthly data values in csv
 meandf.to_csv(r'meandata.csv', index = False)
 
-# plotting
+# *** bar/line plot  ***
+meandf['baseline']=meandf['Mean Temperature'].mean() # set the baseline to yearly/ or monthly mean
+
 plt.rcParams.update({'font.size':15})
-ax=meandf.plot(stacked=False,figsize=(20,10),
-           kind = 'bar',
-           color=['powderblue','orange','red'],
+ax=meandf.plot(x='Month_Year',y=['Minimum Temperature','Mean Temperature','Maximum Temperature','baseline'],
+           stacked=False,figsize=(20,10),kind='line', # (change kind to 'bar' for bar plot)
+           color=['powderblue','orange','red','black'],
            linewidth=3.0,
-           title='Temperature')
+           title='Temperature Data for 2017 to 2021 - Manhattan KS 66502')
 ax.set_ylabel("Average Temperature (°F)")
+
+# **** bar plot with baseline set to Average 5 year mean temperature ****
+# to change Month_Date column to datetime, Do not excute following line when plotting only for January to get nice plot
+# meandf['Month_Year'] = pd.to_datetime(meandf['Month_Year'])
+# custum color 
+domain = ['Minimum Temperature','Mean Temperature','Maximum Temperature']
+range_ = ['powderblue','orange','red']
+
+plot=alt.Chart(meandf).transform_fold(['Minimum Temperature','Mean Temperature','Maximum Temperature'],
+                                ).mark_bar().encode(
+    x='Month_Year',y2='baseline',
+    color=alt.Color('key:N', scale=alt.Scale(domain=domain, range=range_)),
+    column='key:N',
+    y=alt.Y('value:Q', axis=alt.Axis(title='Average Temperature (°F)'))).properties(
+    width=300,height=400,title='Temperature Data for 2017 to 2021 - Manhattan KS 66502')
+plot.configure_title(anchor='middle')
